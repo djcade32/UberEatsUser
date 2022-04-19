cfbf74164d1c15f7abb17603a01da457e8e7b4eb
Norman Cade <norman_cade32@yahoo.com>
2022-04-19 14:24:43 -0400

    working on restaurant Item component.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import RestaurantItem from "./src/components/RestaurantItem/Index";
+import restaurants from "./assets/data/restaurants.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <RestaurantItem restaurant={restaurants[0]} />
+      <RestaurantItem restaurant={restaurants[1]} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
   },
 });
